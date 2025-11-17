b46bb41778df29be2a2d0b55c9f35f4dccebeb05
miles10101 <oyoochris101@gmail.com>
2025-11-17 12:54:57 +0300

    Fixed UI Layout issues in both mobile and desktop view, Added Portal tab

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e290e34d97f3c2dc0c8d55c900d7ea9452c0207b',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e290e34d97f3c2dc0c8d55c900d7ea9452c0207b',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
